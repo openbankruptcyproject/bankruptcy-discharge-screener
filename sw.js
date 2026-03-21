89c489a04a8c77a89c8238ab7e0793bb8d0c01a1
ilikemath9999 <ilikemath9999@users.noreply.github.com>
2026-03-21 13:35:09 -0500

    PWA manifest, service worker, custom 404, dataset schema on state pages

---
+++ sw.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'v1-1328f';
+var CORE_PAGES = [
+  '/',
+  '/index.html',
+  '/explainer.html',
+  '/check.html',
+  '/glossary.html',
+  '/compare.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CORE_PAGES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, clone);
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
